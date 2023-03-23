e78c41924ff9efd05dd5a6c2983e0fc1fc7c6fe1
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2023-03-23 12:52:09 -0300

    Release gtag (#11)
    * Readme and license added
    
    * Deletes LICENSE 2 and lenio logo
    
    * Fixes PR comments
    
    * Deletes lenio logo
    
    * feat: google analitycs 4 (gtag)
    
    * feat: google analytics 4 (gtag)
    
    ---------
    
    Co-authored-by: EstaniPP <estanipp@gmail.com>
    Co-authored-by: Eduardo <edlu.gora96@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import gtag from "../lib/gtag";
+
+gtag.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
